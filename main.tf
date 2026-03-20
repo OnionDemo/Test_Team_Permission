75bcdfe1c6cd1ced9b7bd32fe4c6df3402fbecea
Ting <ting@hashicorp.com>
2026-03-20 12:17:14 -0700

    Add initial Terraform configuration for TFE

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.51"
+    }
+  }
+}
+
+provider "tfe" {
+  hostname = "app.terraform.io"
+}
+
+variable "organization_name" {
+  description = "Name of your HCP Terraform organization"
+  type        = string
+}
+
+variable "team_name" {
+  description = "Name of the team to create"
+  type        = string
+}
+
+resource "tfe_team" "example" {
+  name         = var.team_name
+  organization = var.organization_name
+
+  #Used by Owners and users with "Manage Teams" permissions to control whether team members can manage team tokens. 
+  #Defaults to true. Set to false to test if we can creat team token.
+  allow_member_token_management = false 
+
+  #set this to true, to test what will show in the HCP Terraform UI
+  manage_teams = true
+  visibility = "organization"
+}
